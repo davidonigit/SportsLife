66410ae1c9d3eef7af3adcfe080a24561307f05f
Davi Duarte <93225780+davidonigit@users.noreply.github.com>
2025-04-10 11:35:29 -0300

    First Commit
    Base Spring project with dependencies

---
+++ src/main/java/com/grupo3/sportslife_app/SportslifeAppApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.sportslife_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportslifeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportslifeAppApplication.class, args);
+	}
+
+}
