a4f55de1263bf6c7b51467df7a2e5269bfcf2586
gili-julio <giliardoplay@gmail.com>
2025-04-17 00:28:13 -0300

    fix: adiciona construtores para user

--- src/main/java/com/grupo3/sportslife_app/model/User.java
+++ src/main/java/com/grupo3/sportslife_app/model/User.java
@@ -8,10 +8,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "users")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     
     @Id
