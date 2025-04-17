3a404d7f67de86395e3f46aa7e3c4c8b0aff2a9e
gili-julio <giliardoplay@gmail.com>
2025-04-17 00:28:30 -0300

    fix: adiciona nome ao userDTO

--- src/main/java/com/grupo3/sportslife_app/dto/UserDTO.java
+++ src/main/java/com/grupo3/sportslife_app/dto/UserDTO.java
@@ -1,4 +1,4 @@
 package com.grupo3.sportslife_app.dto;
 
-public record UserDTO(String email, String password, Long userId) {
+public record UserDTO( String name, String email, String password, Long userId) {
 }
